20f76ef71d4b9a3e66c60e09683320d1785a0f1b
Hanqer <hqer@foxmail.com>
2021-02-05 11:42:50 +0800

    a

---
+++ run_scripts/run_agt_trainer_itp.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+DATASET=thumos
+NCLASSES=20
+SR=4
+NSTEPS=1
+EPOCHS=4000
+BATCHSIZE=2
+LR=1e-5
+POSEMB=learned
+LRJOINER=1e-5
+SAVEFREQ=300
+NQUERIES=500
+NENCLAYERS=4
+NDECLAYERS=4
+HDIM=256
+NHEADS=4
+NPOSEMB=512
+DROPOUT=0
+LRDROP=1500
+WDECAY=1e-5
+CLIPNORM=0
+NF=512
+
+OUT="output/checkpoints_"${FOLDER_SUFFIX}"/checkpoints_numqueries"${NQUERIES}"_lr"$LR"_lrdrop"${LRDROP}"_dropout"${DROPOUT}"_clipmaxnorm"${CLIPNORM}"_weightdecay"${WDECAY}"_posemb"$POSEMB"_lrjoiner"${LRJOINER}"_nheads"${NHEADS}"_nenclayers"${NENCLAYERS}"_ndeclayers"${NDECLAYERS}"_hdim"${HDIM}"_sr"${SR}"_batchsize"${BATCHSIZE}"_nposembdict"${NPOSEMB}"_numinputs"${NF}
+
+LOGDIR="output/logs"
+LOG=${LOGDIR}"/log_numqueries"${NQUERIES}"_lr"$LR"_lrdrop"${LRDROP}"_dropout"${DROPOUT}"_clipmaxnorm"${CLIPNORM}"_weightdecay"${WDECAY}"_posemb"$POSEMB"_lrjoiner"${LRJOINER}"_nheads"${NHEADS}"_nenclayers"${NENCLAYERS}"_ndeclayers"${NDECLAYERS}"_hdim"${HDIM}"_sr"${SR}"_batchsize"${BATCHSIZE}"_nposembdict"${NPOSEMB}"_numinputs"${NF}".log"
+
+mkdir -p ${LOGDIR}
+exec &> >(tee -a "${LOG}")
+echo Logging output to "${LOG}"
+
+DATA=$PWD"/../../data/"
+
+python -m torch.distributed.launch --nproc_per_node 8 src/main.py --dataset ${DATASET} --data_root ${DATA} --model "agt" --features "thumos_i3d_rgb" --batch_size ${BATCHSIZE} --enc_layers ${NENCLAYERS} --dec_layers ${NDECLAYERS} --num_queries ${NQUERIES} --nheads ${NHEADS} --dropout ${DROPOUT} --weight_decay ${WDECAY} --clip_max_norm ${CLIPNORM} --num_inputs ${NF} --num_pos_embed_dict ${NPOSEMB} --hidden_dim ${HDIM} --num_workers 0 --num_classes ${NCLASSES} --step_size ${NSTEPS} --sample_rate ${SR}  --lr ${LR} --lr_drop ${LRDROP} --epochs ${EPOCHS} --output_dir ${OUT} --position_embedding ${POSEMB} --lr_joiner ${LRJOINER} --save_checkpoint_every ${SAVEFREQ} --dim_feedforward $(( 4 * ${HDIM} )) --set_cost_segment 5 --set_cost_siou 3 --segment_loss_coef 5 --siou_loss_coef 3 --cuda
+
+
